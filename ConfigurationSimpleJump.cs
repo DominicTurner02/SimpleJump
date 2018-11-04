e97542e51d110ea962771ec53fa7b1deb2df743e
MrKwabs <40438275+MrKwabs@users.noreply.github.com>
2018-11-04 23:54:14 +0000

    Added options for the Ascend and Descend commands.

--- ConfigurationSimpleJump.cs
+++ ConfigurationSimpleJump.cs
@@ -1,14 +1,21 @@
-﻿using Rocket.API;
+using Rocket.API;
 
 namespace SimpleJump
 {
     public class ConfigurationSimpleJump : IRocketPluginConfiguration
     {
         public float MaxJumpDistance;
+        public float MaxAscendDistance;
+        public float MaxDescendDistance;
+        public bool DebugMode;
 
         public void LoadDefaults()
         {
             MaxJumpDistance = 1000f;
+            MaxAscendDistance = 100f;
+            MaxDescendDistance = 100f;
+            DebugMode = false;
+
         }
 
     }
