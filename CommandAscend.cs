8d2514ff27a8a7d10af068ab87e5f10a131a6f6b
MrKwabs <40438275+MrKwabs@users.noreply.github.com>
2018-11-04 23:54:51 +0000

    Added Ascend and Descend commands

---
+++ CommandAscend.cs
@@ -0,0 +1,68 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Player;
+using System.Collections.Generic;
+using SDG.Unturned;
+using Rocket.Unturned.Chat;
+using UnityEngine;
+using System;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace SimpleJump
+{
+    class CommandAscend : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+
+        public string Name => "Ascend";
+
+        public string Help => "Teleports you X meters above you.";
+
+        public string Syntax => "[Distance]";
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>() { "ascend" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            try
+            {
+                UnturnedPlayer uPlayer = (UnturnedPlayer)caller;
+                float playerDirection = uPlayer.Player.look.angle;
+
+                if (command[0].Length == 0)
+                {
+                    UnturnedChat.Say(caller, "Invalid use: /ascend [Distance]", Color.red);
+                    return;
+                }
+
+                ushort Distance = ushort.Parse(command[0]);
+
+                if (Distance <= 0)
+                {
+                    UnturnedChat.Say(caller, "The Distance must be positive!", Color.red);
+                    return;
+                }
+                if (Distance > SimpleJump.Instance.Configuration.Instance.MaxAscendDistance)
+                {
+                    UnturnedChat.Say(caller, $"The maximum Ascend distance is {SimpleJump.Instance.Configuration.Instance.MaxAscendDistance} meters!", Color.red);
+                    return;
+                }
+                Vector3 teleportLocation = new Vector3(uPlayer.Position.x, uPlayer.Position.y + Distance, uPlayer.Position.z);
+                uPlayer.Teleport(teleportLocation, uPlayer.Player.look.angle);
+                UnturnedChat.Say(caller, $"Successfully ascended {Distance} meters.", Color.yellow);
+
+
+            } catch(Exception ex)
+            {
+                UnturnedChat.Say(caller, "Invalid use: /ascend [Distance]", Color.red);
+                if (SimpleJump.Instance.Configuration.Instance.DebugMode)
+                {
+                    Logger.LogWarning($"{ex}");
+                }
+            }
+
+        }
+
+    }
+}
---
+++ CommandDescend.cs
@@ -0,0 +1,67 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Player;
+using System.Collections.Generic;
+using SDG.Unturned;
+using Rocket.Unturned.Chat;
+using UnityEngine;
+using System;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace SimpleJump
+{
+    class CommandDescend : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+
+        public string Name => "Descend";
+
+        public string Help => "Teleports you X meters below you.";
+
+        public string Syntax => "[Distance]";
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>() { "descend" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            try
+            {
+                UnturnedPlayer uPlayer = (UnturnedPlayer)caller;
+                float playerDirection = uPlayer.Player.look.angle;
+
+                if (command[0].Length == 0)
+                {
+                    UnturnedChat.Say(caller, "Invalid use: /descend [Distance]", Color.red);
+                    return;
+                }
+                ushort Distance = ushort.Parse(command[0]);
+                if (Distance <= 0)
+                {
+                    UnturnedChat.Say(caller, "The Distance must be positive!", Color.red);
+                    return;
+                }
+
+                if (Distance > SimpleJump.Instance.Configuration.Instance.MaxAscendDistance)
+                {
+                    UnturnedChat.Say(caller, $"The maximum Descend distance is {SimpleJump.Instance.Configuration.Instance.MaxDescendDistance} meters!", Color.red);
+                    return;
+                }   
+
+                Vector3 teleportLocation = new Vector3(uPlayer.Position.x, uPlayer.Position.y - Distance, uPlayer.Position.z);
+                uPlayer.Teleport(teleportLocation, uPlayer.Player.look.angle);
+                UnturnedChat.Say(caller, $"Successfully descended {Distance} meters.", Color.yellow);
+            }
+            catch (Exception ex)
+            {
+                UnturnedChat.Say(caller, "Invalid use: /descend [Distance]", Color.red);
+                if (SimpleJump.Instance.Configuration.Instance.DebugMode)
+                {
+                    Logger.LogWarning($"{ex}");
+                }
+            }
+
+        }
+
+    }
+}
